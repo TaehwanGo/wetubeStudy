2457884680f61384650f27022df46f94310c66e5
TaehwanGo <gth1123@naver.com>
2020-11-18 17:10:10 +0900

    2.3 express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello world !");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+// 서버를 콜백으로 바꾸기
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening); // 포트 4000번을 리슨
