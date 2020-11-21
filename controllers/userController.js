1878a74404250fbcb8f815b20ccfc8a7d0ca086d
TaehwanGo <gth1123@naver.com>
2020-11-22 02:06:47 +0900

    2.24 Login and user profile controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,7 +20,11 @@ export const postJoin = (req, res) => { // /join에 post 방식에 해당하는 
         res.redirect(routes.home);
     }
 }
-export const login = (req, res) => res.render("login", {pageTitle:'Login'});
+export const getLogin = (req, res) => res.render("login", {pageTitle:'Login'});
+export const postLogin = (req, res) => {
+    res.redirect(routes.home); // successful // 나중엔 DB에 있는 비밀번호와 같은지 검사해야 함 
+    // 나중에 authentication(사용자 인증)에 문제가 생기면 다시 login 화면을 표시 하게 할 예정 
+}
 export const logout = (req, res) => res.render("logout", {pageTitle:'Logout'});
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle:'User Detail'});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle:'Edit Profile'});
--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,9 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "Wetube";
     res.locals.routes = routes;
+    res.locals.user = { // 임시 데이터, 나중에 DB에 직접 데이터를 저장하고 authentication을 만들 예정 
+        isAuthenticated: true,
+        id: 1
+    }
     next(); //- 미들웨어가 next에 req를 전달해야 함, 이 경우 미들웨어가 커넥션과 라우트들 사이에 있으므로 next()를 반드시 추가
 }
