235de1340b5d54e3e94878e7b905886e28f8d241
TaehwanGo <gth1123@naver.com>
2020-11-25 18:26:38 +0900

    3.5 async and await, try and catch

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,17 @@
 // import {videos} from "../db"; // 더 이상 controller에서 import하는게 아닌 init.js에서 import 해서 사용 할 것임
 import routes from "../routes"
+import Video from "../models/Video"; // 이건 단지 model 이고 dattabase의 element가 아님 
 
-export const home = (req, res) => {
+export const home = async (req, res) => { // async가 없다면 videos를 발견 못 하면 그냥 render 함
+    try{ // 기다리긴 하지만 error가 발생한 경우도 success로 간주하여 render로 넘어가기 때문에 try catch를 적용 
+        const videos = await Video.find({}); // 이 find은 모든, 어떤 video든 가져올 것임, await은 오직 async function안에서만 가능함
+        res.render("home", {pageTitle:'Home', videos}); // res.send("Home") 대신 html을 보내기 위해 render로 수정, /views가 default 폴더이기 때문에 따로 import하지 않아도 자동으로 찾아서 전송함
+    } catch(error) {
+        console.log(error);
+        res.render("home", {pageTitle:'Home', videos});
+    }
+    // console.log(videos);
     // home화면에 비디오 목록을 띄울 것 임
-    res.render("home", {pageTitle:'Home', videos}); // res.send("Home") 대신 html을 보내기 위해 render로 수정, /views가 default 폴더이기 때문에 따로 import하지 않아도 자동으로 찾아서 전송함
 }
 export const search = (req, res) => {
     // const searchingBy = req.query.term; // ES6 방식
