95c69c2a72c5e2587b8d1ab8e8691b7172d8a6d9
TaehwanGo <gth1123@naver.com>
2020-11-28 04:38:29 +0900

    3.11 Installing ESLint

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,11 +4,13 @@ import Video from "../models/Video"; // 이건 단지 model 이고 dattabase의 
 
 export const home = async (req, res) => { // async가 없다면 videos를 발견 못 하면 그냥 render 함
     try{ // 기다리긴 하지만 error가 발생한 경우도 success로 간주하여 render로 넘어가기 때문에 try catch를 적용 
-        const videos = await Video.find({}); // 이 find은 모든, 어떤 video든 가져올 것임, await은 오직 async function안에서만 가능함
+        // 이 find은 모든, 어떤 video든 가져올 것임, await은 오직 async function안에서만 가능함
+        const videos = await Video.find({}).sort({'_id': -1}); // 역순으로 정렬 -1은 순서를 위아래를 바꾸겠다는 약속같은 것 
         res.render("home", {pageTitle:'Home', videos}); // res.send("Home") 대신 html을 보내기 위해 render로 수정, /views가 default 폴더이기 때문에 따로 import하지 않아도 자동으로 찾아서 전송함
+        // console.log(next);
     } catch(error) {
         console.log(error);
-        res.render("home", {pageTitle:'Home', videos});
+        res.render("home", {pageTitle:'Home', videos: []});
     }
     // console.log(videos);
     // home화면에 비디오 목록을 띄울 것 임
@@ -19,7 +21,7 @@ export const search = (req, res) => {
         query: {term: searchingBy} // term에 searchingBy라는 새로운 이름 부여
     } = req; // 이게 왜 더 좋은 방식이지? 
     // console.log(searchingBy);
-    res.render("search", {pageTitle:'Search', searchingBy, videos}); // searchingBy: searchingBy를 간단히 할 수 있음
+    res.render("search", {pageTitle:'Search', searchingBy}); // searchingBy: searchingBy를 간단히 할 수 있음
 }
 
 export const getUpload = (req, res) => res.render("upload", {pageTitle:'Upload'});
@@ -84,6 +86,8 @@ export const deleteVideo = async (req, res) => {
     } = req;
     try{
         await Video.findOneAndRemove({_id:id});
-    } catch(error) {}
+    } catch(error) {
+        console.log(error);
+    }
     res.redirect(routes.home); // try와 catch가 공통적으로 redirect home이라서 밖으로 뺌
 }
---
+++ test.js
@@ -0,0 +1,41 @@
+// ** http server **
+const http = require('http')
+
+// const port = process.env.PORT
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('<h1>Hello, World!</h1>')
+})
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
+
+
+
+
+// ** Perform a GET Request **
+const https = require('http')
+const options = {
+  hostname: 'localhost',
+  port: 4000,
+  path: '/',
+  method: 'GET'
+}
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+console.log(req);
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
