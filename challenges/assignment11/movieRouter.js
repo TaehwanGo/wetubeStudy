cb98a00e863c29c9e37e3d2cc27ce8a40add8d99
TaehwanGo <gth1123@naver.com>
2020-12-01 18:52:10 +0900

    upload page

---
+++ challenges/assignment11/movieRouter.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  getAddMovie,
+  home,
+  movieDetail,
+  postAddMovie
+} from "./movieController";
+
+const movieRouter = express.Router();
+
+movieRouter.get("/", home);
+/*
+Here add a way to handle GET and POST requests to the "/add" URL
+Make sure is ABOVE /:id or it WON'T work.
+*/
+
+movieRouter.get("/add", getAddMovie);
+movieRouter.post("/add", postAddMovie);
+
+movieRouter.get("/:id", movieDetail);
+
+export default movieRouter;
