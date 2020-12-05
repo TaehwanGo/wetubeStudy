42d18df21bd4fa60245573dcf439f17c2fa36c13
TaehwanGo <gth1123@naver.com>
2020-12-05 20:10:03 +0900

    full screen and exit full screen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -4,6 +4,9 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video"); // 방법2 : container에 id부여해서 if문으로 다같이 검사 후 그 안에 있는 video태그를 가져옴
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeButton");
+const fullScreenBtn = document.getElementById("jsFullScreen");
+
+
 
 function handlePlayClick(){
     if(videoPlayer.paused){
@@ -26,11 +29,25 @@ function handleVolumeClick() {
     }
 }
 
+function exitFullScreen() {
+    fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
+    fullScreenBtn.addEventListener("click", goFullScreen);
+    document.exitFullscreen();
+}
+
+function goFullScreen() {
+    videoContainer.requestFullscreen(); // videoPlayer로 하는 것과 차이점 : 그래야 우리가 만든 컨트롤버튼들만 동작하게 할 수 있음
+    fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+    fullScreenBtn.removeEventListener("click", goFullScreen);
+    fullScreenBtn.addEventListener("click", exitFullScreen);
+}
+
 function init(){
     // 우리가 원하는 기능적인 부분 추가 - adding, finding 변수를 더 할 에정(아직 이해 안됨)
     // videoPlayer = videoContainer.querySelector("video"); // const로 이 안에서 선언 시 다른 함수에서 사용 불가능하기 때문에 밖에서 let으로 선언함 // 방법1
     playBtn.addEventListener("click", handlePlayClick)
     volumeBtn.addEventListener("click", handleVolumeClick);
+    fullScreenBtn.addEventListener("click", goFullScreen);
 }
 
 if(videoContainer){
