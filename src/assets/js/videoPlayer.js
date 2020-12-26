9cc9a3e00f9adcdea340c7bfd9ebb11e1bb4fd3a
TaehwanGo <gth1123@naver.com>
2020-12-26 13:46:02 +0900

    videoTotal time

--- src/assets/js/videoPlayer.js
+++ src/assets/js/videoPlayer.js
@@ -148,6 +148,7 @@ async function setTotalTime() {
     //     totalTimeString = formatDate(videoPlayer.duration);
     // }
     // else {
+        console.log(videoPlayer.duration);
         const blob = await fetch(videoPlayer.src).then(response => response.blob()); // fetch는 http통신용인줄 알았는데
         // videoPlayer.src를 받아서 그걸 서버에 요청해서 응답을 받으면 response.blob반환(blob는 파일)
         const duration = await getBlobDuration(URL.createObjectURL(blob));
@@ -256,11 +257,12 @@ function init(){
     videoPlayer.volume = 0.5;
     videoRange.value = 0;
     videoRange.max = Math.floor(videoPlayer.duration);
-    // setTotalTime(); // 비디오 플레이어에 토탈 시간 수정 필요
+    setTotalTime(); // 비디오 플레이어에 토탈 시간 수정 필요
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
     fullScreenBtn.addEventListener("click", goFullScreen);
-    videoPlayer.addEventListener("loadedmetadata", setTotalTime); // 느리면 이 이벤트가 감지가 안되는것 같다. 
+    // videoPlayer.addEventListener("loadedmetadata", setTotalTime); // 느리면 이 이벤트가 감지가 안되는것 같다. 
+    // videoPlayer.addEventListener("onload", setTotalTime); // 느리면 이 이벤트가 감지가 안되는것 같다. 
     videoPlayer.addEventListener("timeupdate", updateTime);
     videoPlayer.addEventListener("ended", handleEnded);
     videoPlayer.addEventListener("mouseenter", () => {
